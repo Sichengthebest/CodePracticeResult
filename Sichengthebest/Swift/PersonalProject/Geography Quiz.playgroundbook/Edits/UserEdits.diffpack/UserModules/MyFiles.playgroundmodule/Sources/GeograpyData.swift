ae4fab43a79b6e9502484eb497186b09e523869d
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2025-11-16 21:23:29 -0500

    geographygame

---
+++ Sichengthebest/Swift/PersonalProject/Geography Quiz.playgroundbook/Edits/UserEdits.diffpack/UserModules/MyFiles.playgroundmodule/Sources/GeograpyData.swift
@@ -0,0 +1,185 @@
+public var capitalQ: [String:[String:[String:[String]]]] = [
+    "Easy":[
+        "🇫🇷 France 🇫🇷" : [
+            "yes":["Paris"],
+            "no":["Calais","Marseilles","Versailles","Québec"],
+            "haha":["thE eIFFel ToWEr obVioUSlY","Macron City","Beaconsfield"]
+        ],
+        "🇨🇦 Canada 🇨🇦" : [
+            "yes":["Ottawa"],
+            "no":["Toronto","Kingston","Vancouver"],
+            "haha":["The North Pole HOHOHO","Beaconsfield"]
+        ],
+        "🇨🇳 China 🇨🇳" : [
+            "yes":["Beijing"],
+            "no":["Shanghai","Beijiing","Guangzhou"],
+            "haha" : ["Centre City","Beaconsfield"]
+        ],
+        "🇬🇧 UK 🇬🇧" : [
+            "yes":["London"],
+            "no":["Windsor","Oxford","Cambridge","Manchester"],
+            "haha":["Small Ben?","Beaconsfield"]
+        ],
+        "🇧🇷 Brazil 🇧🇷" : [
+            "yes":["Brasília"],
+            "no": ["São Paulo","Rio de Janeiro","Fortaleza"],
+            "haha": ["Amazonia","Where the 2016 Olympics were held?","Beaconsfield"]
+        ],
+        "🇰🇷 South Korea 🇰🇷" : [
+            "yes":["Seoul"],
+            "no": ["Busan", "Gwangzhou", "Incheon"],
+            "haha": ["A Place Where Kim-Jong-Un hates?","Beaconsfield"]
+        ],
+        "🇷🇺 Russia 🇷🇺": [
+            "yes":["Moscow"],
+            "no": ["St-Petersburg","Arkhangelsk","Volgograd"],
+            "haha": ["Vladimirputingrad","USSR II City","Beaconsfield"]
+        ],
+        "🇯🇵 Japan 🇯🇵": [
+            "yes":[ "Tokyo"],
+            "no": ["Osaka","Nagasaki","Hiroshima","Fukushima"],
+            "haha": ["Toyota City", "Beaconsfield"]
+        ],
+        "🇺🇸 USA 🇺🇸": [
+            "yes":[ "Washington DC"],
+            "no": ["New York", "New Youk","Washington DD","Philadelphia"],
+            "haha": ["The Place Where There Is A Very Messy Election?","Beaconsfield"]
+        ],
+        "🇮🇳 India 🇮🇳": [
+            "yes":["New Delhi"],
+            "no":["Delhi","Calcutta","Mumbai"],
+            "haha":["Kashmere","Beaconsfield"]
+        ]
+    ],
+    "Normal":[
+        "🇸🇪 Sweden 🇸🇪" : [
+            "yes":[ "Stockholm"],
+            "no":["Hallashölm","Göteborg","Umeå"],
+            "haha":["Malmö","Copenhaguen"]
+        ],
+        "🇳🇴 Norway 🇳🇴": [
+            "yes":[ "Oslo"],
+            "no":["Helsinki","Bergen","Trondheim"],
+            "haha":["Bodø","Tromsø"]
+        ],
+        "🇫🇮 Finland 🇫🇮": [
+            "yes":[ "Helsinki"],
+            "no": ["Turku","Oslo","Joensuu"],
+            "haha": ["Oulu","Tampere"]
+        ],
+        "🇦🇺 Australia 🇦🇺": [
+            "yes":[ "Canberra"],
+            "no": ["Sydney","Melbourne"],
+            "haha": ["Perth","Brisbane"]
+        ],
+        "🇩🇰 Denmark 🇩🇰": [
+            "yes":[ "Copenhaguen"],
+            "no": ["Stockholm","Aarhus","Odense"],
+            "haha": ["Aalborg","Esbjerg"]
+        ],
+        "🇩🇿 Algeria 🇩🇿" : [
+            "yes":[ "Algiers"],
+            "no": ["Ouargla","Constantine","Bejaïa"],
+            "haha": ["Annaba","Oran"]
+        ],
+        "🇵🇪 Peru 🇵🇪" : [
+            "yes":[ "Lima"],
+            "no": ["Trujillo","Cuzco"],
+            "haha": ["Areqipa","Iquitos"]
+        ],
+        "🇳🇿 New Zealand 🇳🇿": [
+            "yes":[ "Wellington"],
+            "no": ["Auckland","Hamilton"],
+            "haha": ["Christchurch","Dunedin"]
+        ],
+        "🇵🇹 Portugal 🇵🇹": [
+            "yes":[ "Lisbon"],
+            "no": ["Porto","Almendralejo"],
+            "haha": ["Faro","Coimbra"]
+        ],
+        "🇮🇷 Iran 🇮🇷": [
+            "yes":[ "Tehran"],
+            "no": ["Baghdad","Tabriz","Isfahan"],
+            "haha": ["Shiraz","Zahedan"]
+        ]
+    ],
+    "Hard":[
+        "🇻🇪 Venezuela 🇻🇪" : [
+            "yes":["Caracas"],
+            "no":["Maracaibo","Barquisimeto","Venezuelia"],
+            "haha":["Valencia"]
+        ],
+        "🇨🇴 Colombia 🇨🇴": [
+            "yes":[ "Bogotá"],
+            "no":["Barranquilla","Medellín","Cartagena"],
+            "haha":["Cali","Montería"]
+        ],
+        "🇺🇦 Ukraine 🇺🇦": [
+            "yes":[ "Kiev"],
+            "no": ["Odessa", "Dnipropetrovsk","Kharkiv"],
+            "haha": ["Chernobyl","Zaporizhia"]
+        ],
+        "🇱🇻 Latvia 🇱🇻": [
+            "yes":[ "Riga"],
+            "no": ["Daugavpils","Liepāja"],
+            "haha": ["Ventspils","Rēzekne"]
+        ],
+        "🇬🇪 Georgia 🇬🇪": [
+            "yes":[ "Tbilisi"],
+            "no" : ["Kutaisi","Batumi"],
+            "haha" : ["Borjomi","Rustavi"] 
+        ],
+        "🇨🇬 Congo 🇨🇬": [
+            "yes":[ "Brazzaville"],
+            "no": ["Kinshasa","Pointe-Noire","Dolisie"],
+            "haha": ["Libreville","Mbandaka"]
+        ],
+        "🇨🇱 Chile 🇨🇱": [
+            "yes":[ "Santiago de Chile"],
+            "no": ["Antofagasta","Concepción","Valparaíso"],
+            "haha": ["Punta Arenas","Copiapó"]
+        ],
+        "🇵🇱 Poland 🇵🇱": [
+            "yes":[ "Warsaw"],
+            "no": ["Wrocław","Szczecin","Gdańsk"],
+            "haha": ["Lódź","Lublin"]
+        ]
+    ],
+    "Extreme":[
+        "🇵🇬 Papua New Guinea 🇵🇬" : [
+            "yes":[ "Port Moresby"],
+            "no":["Daru","Lae"],
+            "haha":["Jayapura"]
+        ],
+        "🇸🇮 Slovenia 🇸🇮" : [
+            "yes":[ "Ljubljana"],
+            "no":["Bratislava","Maribor"],
+            "haha":["Velenje","Koper"]
+        ],
+        "🇲🇰 North Macedonia 🇲🇰": [
+            "yes":["Skopje"],
+            "no": ["Podgorica","Bitola"],
+            "haha": ["Ohrid","Prilep"]
+        ],
+        "🇲🇪 Montenegro 🇲🇪": [
+            "yes":[ "Podgorica"],
+            "no": ["Skopje","Nikšić"],
+            "haha": ["Berane","Kotor"]
+        ],
+        "🇱🇹 Lithuania 🇱🇹": [
+            "yes":[ "Vilnius"],
+            "no": ["Kaunas","Šiauliai","Klaipėda"],
+            "haha": ["Panevėžys","Palanga"]
+        ],
+        "🇧🇯 Benin 🇧🇯": [
+            "yes":[ "Porto-Novo"],
+            "no": ["Parakou","Natitinqou"],
+            "haha": ["Cotonou","Kandi"]
+        ],
+        "🇸🇰 Slovakia 🇸🇰": [
+            "yes":[ "Bratislava"],
+            "no": ["Košice","Ljubljana"],
+            "haha": ["Lučenec","Zvolen","Nové Zámky"]
+        ]
+    ]
+]
